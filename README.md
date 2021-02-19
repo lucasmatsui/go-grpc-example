75358c42c7145f2627f2ea4d711067fe0e441b82
Lucas Matsui <lucas-matsui@hotmail.com>
2021-02-18 23:26:50 -0300

    Implementando uma comunicação bidirecional

--- README.md
+++ README.md
@@ -7,4 +7,5 @@
 	- Adiciona um usuário (**unary**)
 	- Adiciona um usuário dando feedback de status de inserção em tempo real para o client (**server streaming**)
 	- Adiciona vários usuários dando feedback de criação em tempo real para o server (**client streaming**)
+	- Adiciona vários usuários mandando e recebendo os feedbacks de inserção (**client and server streaming**)
 - Um **client** que faz as chamada gRPC
--- cmd/client/client.go
+++ cmd/client/client.go
@@ -26,8 +26,9 @@ func main() {
 
 	//AddUserVerbose(client)
 
-	AddUsers(client)
+	//AddUsers(client)
 
+	AddUserStreamBoth(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -125,4 +126,72 @@ func AddUsers(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserStreamBoth(client pb.UserServiceClient) {
+
+	stream, err := client.AddUserStreamBoth(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	reqs := []*pb.User{
+		{
+			Id:    "k1",
+			Name:  "Kenzo",
+			Email: "ken@ken.com",
+		},
+		{
+			Id:    "k2",
+			Name:  "Kenzo 2",
+			Email: "ken2@ken.com",
+		},
+		{
+			Id:    "k3",
+			Name:  "Kenzo 3",
+			Email: "ken3@ken.com",
+		},
+		{
+			Id:    "k4",
+			Name:  "Kenzo 4",
+			Email: "ken4@ken.com",
+		},
+		{
+			Id:    "k5",
+			Name:  "Kenzo 5",
+			Email: "ken5@ken.com",
+		},
+	}
+
+	wait := make(chan int)
+
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending user: ", req.GetName())
+			stream.Send(req)
+			time.Sleep(time.Second * 2)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				log.Fatalf("Error receiving data: %v", err)
+			}
+
+			fmt.Printf("Recebendo user %v com status: %v \n", res.User.GetName(), res.GetStatus())
+		}
+
+		close(wait)
+	}()
+
+	<-wait
 }
